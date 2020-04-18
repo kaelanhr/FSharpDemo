d406d7b3d0beaa658107d23c67deca1caaa8dc3f
Kael <kaelanreece@gmail.com>
2020-04-18 11:18:34 +1000

    add initial demo code

---
+++ NumberOperations.fs
@@ -0,0 +1,10 @@
+﻿namespace NumberOperations
+
+module Exponential =
+    let Exponent (x:float, y:float) = x**y
+    let SquareNumber (x:float) = Exponent (x, 2.0)
+    let AddNumber x y = x + y
+    let inline MultiplyNumber (x, y) = x * y
+    let inline DivideNumber (x, y) = x / y
+    let GetPercentage (fraction:float, total:float) = MultiplyNumber (DivideNumber (fraction, total), 100.0)
+    let GetPercentageString (fraction:float, total:float) = (GetPercentage (fraction, total)).ToString() + "%"
---
+++ Program.fs
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open NumberOperations
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    printfn "%f" (Exponential.SquareNumber (3.0))
+    printfn "%f" (Exponential.Exponent (3.0, 2.0))
+    printfn "%s" (Exponential.GetPercentageString(50.0, 100.0));
+    printfn "%s" (Exponential.GetPercentageString(23.4, 300.0));
+    0 // return an integer exit code
